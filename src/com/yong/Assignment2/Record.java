7aafc79de52d03275adb89dfb204483ba3433656
yymin1022 <yymin1022@gmail.com>
2020-03-28 19:28:25 +0900

    Update Assignment2 Base Code

--- src/com/yong/Assignment2/Record.java
+++ src/com/yong/Assignment2/Record.java
@@ -1,4 +1,22 @@
 package com.yong.Assignment2;
 
 public class Record {
+    int IdNumber;
+    int Korean;
+    int Math;
+    int English;
+    int Science;
+    int sum;
+
+
+    public Record(int x, int y, int z, int a, int b ) {
+        IdNumber = x;
+        Korean = y;
+        Math = z;
+        English =a;
+        Science = b;
+    }
+    public int getIdNumber () {
+        return IdNumber;
+    }
 }
